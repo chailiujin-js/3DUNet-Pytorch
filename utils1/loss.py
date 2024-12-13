09585d9b294ed4f8852cc689d82ca1239d7b3b38
Your Name <you@example.com>
2024-12-13 16:13:56 +0800

    chore: delete redundant files

--- utils1/loss.py
+++ utils1/loss.py
@@ -112,52 +112,6 @@ class TverskyLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    # def forward(self, pred, target):
-    #
-    #     smooth = 1
-    #
-    #     dice = 0.
-    #
-    #     for i in range(pred.size(1)):
-    #         target = target.squeeze(0)
-    #         print("pred:", pred.size())
-    #         print("target:", target.size())
-    #
-    #
-    #         # print(pred.size)
-    #         dice += (pred[:,i] * target[:,i]).sum(dim=1).sum(dim=1).sum(dim=1) / ((pred[:,i] * target[:,i]).sum(dim=1).sum(dim=1).sum(dim=1)+
-    #                     0.3 * (pred[:,i] * (1 - target[:,i])).sum(dim=1).sum(dim=1).sum(dim=1) + 0.7 * ((1 - pred[:,i]) * target[:,i]).sum(dim=1).sum(dim=1).sum(dim=1) + smooth)
-    #
-    #
-    #     dice = dice / pred.size(1)
-    #     return torch.clamp((1 - dice).mean(), 0, 2)
-
-    # def forward(self, pred, target):
-    #     smooth = 1
-    #     dice = 0.
-    #
-    #     # 确保 pred 和 target 的尺寸匹配
-    #     if pred.size()[1:] != target.size()[1:]:
-    #         target = target.squeeze(0)  # 去掉 batch 维度
-    #
-    #     # 确保 pred 和 target 的通道数一致
-    #     if pred.size(1) != target.size(1):
-    #         raise ValueError("pred and target must have the same number of channels")
-    #
-    #     print("pred:", pred.size())
-    #     print("target:", target.size())
-    #
-    #     for i in range(pred.size(1)):
-    #         # 计算每个通道的 Tversky 损失
-    #         dice += (pred[:, i].squeeze(1) * target[:, i]).sum(dim=1).sum(dim=1) / (
-    #                 (pred[:, i].squeeze(1) * target[:, i]).sum(dim=1).sum(dim=1) +
-    #                 0.3 * (pred[:, i].squeeze(1) * (1 - target[:, i])).sum(dim=1).sum(dim=1) +
-    #                 0.7 * ((1 - pred[:, i].squeeze(1)) * target[:, i]).sum(dim=1).sum(dim=1) + smooth
-    #         )
-    #
-    #     dice = dice / pred.size(1)
-    #     return torch.clamp((1 - dice).mean(), 0, 2)
-
     def forward(self, pred, target):
         smooth = 1
         dice = 0.
